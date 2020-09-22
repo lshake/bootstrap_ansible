0e0204f6d78d4666d018f87a74b0076d89387bc2
Will McDonald <wmcdonald@gmail.com>
2020-09-22 21:13:08 +0100

    Tweaked build.sh to mitigate https://github.com/ansible-community/molecule/issues/1724 during playbook execution. -wmcd

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 . ~/local/python/bootstrap/bin/activate
-tox --notest -c $(dirname $0)
+if [ -f /etc/redhat-release ]
+then
+  tox --sitepackages --notest -c $(dirname $0)
+else
+  tox --notest -c $(dirname $0)
+fi
